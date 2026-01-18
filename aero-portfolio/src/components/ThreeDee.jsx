b92208681dd59d5adabff6717c40badbba745d52
Jack <jack@appa.localdomain>
2026-01-17 19:33:23 -0600

    feat: added content

--- aero-portfolio/src/components/ThreeDee.jsx
+++ aero-portfolio/src/components/ThreeDee.jsx
@@ -1,21 +1,36 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Environment } from '@react-three/drei'
 import Model from './Model.jsx'
-import { useRef, useState, useEffect } from 'react'
+import { CamContext } from '../context/contexts.jsx'
+import { useRef, useState, useContext, useEffect } from 'react'
 
 function CameraRig() {
-  const [scrollY, setScrollY] = useState(0)
+  const {camPos, setCamPos} = useContext(CamContext);
+  const easedPos = useRef(0);
 
   useEffect(() => {
-    const onScroll = () => setScrollY(window.scrollY)
+    const onScroll = () => setCamPos(window.scrollY)
     window.addEventListener('scroll', onScroll)
     return () => window.removeEventListener('scroll', onScroll)
   }, [])
 
+  // useFrame(({ camera }) => {
+  //   camera.position.x = 5 - scrollY * -0.01
+  //   camera.position.z = 5 - scrollY * -0.02
+  // })
   useFrame(({ camera }) => {
-    camera.position.x = 5 - scrollY * -0.01
-    camera.position.z = 5 - scrollY * -0.02
+    camera.position.x = 5 - camPos * -0.01
+    camera.position.z = 5 - camPos * -0.02
   })
+  // useFrame((_, delta) => {
+  //   const ease = 1 - Math.exp(-6 * delta)
+  //   easedPos.current += (camPos - easedPos.current) * ease
+
+  //   const s = easedPos.current
+
+  //   _.camera.position.x = 5 - s * -0.01
+  //   _.camera.position.z = 5 - s * -0.02
+  // })
 
   return null
 } 
---
+++ aero-portfolio/src/context/contexts.jsx
@@ -0,0 +1,7 @@
+import { createContext, useContext, useState } from "react";
+
+const CamContext = createContext(null);
+
+export {
+    CamContext
+}
